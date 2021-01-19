a88113df84f72ff5e75673a7bd58ecd03bffb09a
Yuri Sulyma <yuri@lmqm.xyz>
2021-01-19 02:07:04 -0500

    listen on 8080

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ const app = express();
 app.use("/", express.static(__dirname));
 
 app.set("port", 8080);
-// app.listen(app.get("port"));
+app.listen(app.get("port"));
 
 console.log("Listening on port " + app.get("port"));
