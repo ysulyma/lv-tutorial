45f39be9efa27d1648a3533c2c73106cd00c55a4
Yuri Sulyma <yuri@lmqm.xyz>
2021-03-04 20:24:46 -0500

    Remove LiveReload https code

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const fs = require("fs");
+
 const livereload = require("livereload");
 
 // livereload
 const lrServer = livereload.createServer({
-  https: {
-    key: fs.readFileSync(`${__dirname}/ssl/ractive-player.key`, "utf-8"),
-    cert: fs.readFileSync(`${__dirname}/ssl/ractive-player.crt`, "utf-8")
-  },
+  /*
+    uncomment this to use LiveReload over https
+    (https is required to record audio)
+  */
+
+  // https: {
+  //   key: fs.readFileSync(`${__dirname}/ssl/ractive-player.key`, "utf-8"),
+  //   cert: fs.readFileSync(`${__dirname}/ssl/ractive-player.crt`, "utf-8")
+  // },
   port: 35729
 });
 
